41c208d181184457833d6ad62763e67acbfadce7
afzalmasra125 <afzalmasra@gmail.com>
2018-01-14 13:52:27 -0600

    yo

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def sunday
+    render json: {message:"sunday"}
+end
+
+  def monday
+    render json: {message:"sunday"}
+end
+  def tuesday
+    render json: {message:"sunday"}
+end
+end 
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+ get "/practice1" => 'pages#sunday'
+ get "/practice2" => 'pages#monday'
+ get "/practice2" => 'pages#tuesday'
+end
